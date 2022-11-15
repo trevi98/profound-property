ee3354a6e7cf6fca0ff2c2776f4c551436b7fbde
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-11-15 23:21:15 +0400

    logo test

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="text-[#f00]">
-        {{message}}
-    </div>
+    <Header/>
     <router-link :to="{name:'xx'}">Go to Foo</router-link>
     <router-view/>
 
 </template>
 <script>
+import Header from './components/Header.vue';
 export default {
+    components:{Header},
     data() {
         return {
             message: 'Hello World'
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-between items-center w-[90%] mx-auto h-[200px]">
+    <div>
+        <img src="../../assets/w-logo.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
